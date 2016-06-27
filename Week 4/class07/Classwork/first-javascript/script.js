4d256a0e4e5167fd1a3aae25b415fafae8812c90
jillianmatt <jem733@nyu.edu>
2016-06-27 19:46:49 -0400

    Week 4
    Week 4

---
+++ Week 4/class07/Classwork/first-javascript/script.js
@@ -0,0 +1,5 @@
+alert('this is an alert. this is annoying');
+
+	document.write('<h2>writing to the document, like a boss</h2>');
+
+	// document.write('<h3>"Time is what keeps everyting form heppening at once"<h3>')')
